952961738e073f67b171f91da98f0c09141a4f85
brendanmc6 <brendanmc6@users.noreply.github.com>
2024-12-15 19:31:00 -0800

    Day04_P01

--- lib/Day04_P01.ex
+++ lib/Day04_P01.ex
@@ -1,13 +1,29 @@
 defmodule Grid do
-  defstruct map: %{}, max_x: 0, max_y: 0
+  defstruct map: %{}, max_x: 0, max_y: 0, count: 0
 
-  @type t :: %__MODULE__{map: map(), max_x: integer(), max_y: integer()}
+  @type t :: %__MODULE__{map: map(), max_x: integer(), max_y: integer(), count: integer()}
 end
 
 defmodule Day04_P01 do
   @type rows_list :: [String.t()]
+  @type puzzle_input :: Grid.t()
+  @type coord :: {integer(), integer()}
+  @type vector :: {integer(), integer()}
 
   @behaviour Common.Solver
+
+  @word "XMAS"
+  @word_length String.length(@word)
+  @vectors [
+    {:n, {0, -1}},
+    {:ne, {1, -1}},
+    {:e, {1, 0}},
+    {:se, {1, 1}},
+    {:s, {0, 1}},
+    {:sw, {-1, 1}},
+    {:w, {-1, 0}},
+    {:nw, {-1, -1}}
+  ]
 
   @spec read(String.t()) :: rows_list()
   def read(path) do
@@ -41,7 +57,36 @@ defmodule Day04_P01 do
     |> Enum.reduce(initial_grid, &reduce_row/2)
   end
 
-  def solve(puzzle_input) do
-    puzzle_input
+  def match(grid, {x, y}, {vx, vy}) do
+    0..(@word_length - 1)
+    # Optimistically get the value at each coordinate, even if out of bounds
+    |> Enum.map(fn i ->
+      Map.get(grid.map, {x + vx * i, y + vy * i})
+    end)
+    |> Enum.join("")
+    |> then(&if &1 == @word, do: 1, else: 0)
+  end
+
+  @spec count(vector(), Grid.t(), coord()) :: Grid.t()
+  def count(vector, grid, coords \\ {0, 0})
+
+  # Base case: vector y crosses bottom boundary, finish count
+  def count({_vx, vy}, grid, {_x, y}) when y + vy * (@word_length - 1) > grid.max_y, do: grid
+
+  # Recursive case: parsed entire row, move to next row
+  def count({vx, _vy} = vector, grid, {x, y}) when x + vx * (@word_length - 1) > grid.max_x,
+    do: count(vector, grid, {0, y + 1})
+
+  # Recursive case: check for match and increment count
+  def count(vector, grid, {x, y} = coords) do
+    newCount = grid.count + match(grid, coords, vector)
+    count(vector, %{grid | count: newCount}, {x + 1, y})
+  end
+
+  @spec solve(Grid.t()) :: integer()
+  def solve(grid) do
+    @vectors
+    |> Enum.reduce(grid, fn {_dir, v}, g -> count(v, g) end)
+    |> then(& &1.count)
   end
 end
--- test/Day04_P01_test.exs
+++ test/Day04_P01_test.exs
@@ -10,4 +10,92 @@ defmodule Day04_P01.Test do
     assert Map.get(grid.map, {0, 0}) == "X"
     assert Map.get(grid.map, {3, 3}) == "S"
   end
+
+  test "count_south_east" do
+    input1 = [
+      "XX...",
+      ".MM..",
+      "..AA.",
+      "...SS"
+    ]
+
+    grid = count({1, 1}, parse(input1), {0, 0})
+    assert grid.count == 2
+  end
+
+  test "count_north_west" do
+    input1 = [
+      ".S...",
+      ".SA..",
+      "..AM.",
+      "...MX",
+      "....X"
+    ]
+
+    grid = count({-1, -1}, parse(input1), {0, 0})
+    assert grid.count == 2
+    assert grid.max_x == 4
+    assert grid.max_y == 4
+  end
+
+  test "count_south" do
+    input1 = [
+      "XS...",
+      "MSA.X",
+      "A.AMM",
+      "S..MA",
+      "....S"
+    ]
+
+    grid = count({0, 1}, parse(input1), {0, 0})
+    assert grid.count == 2
+    assert grid.max_x == 4
+    assert grid.max_y == 4
+  end
+
+  test "solve_all" do
+    input1 = [
+      "S..S..S",
+      ".A.A.A.",
+      "..MMM..",
+      "SAMXMAS",
+      "..MMM..",
+      ".A.A.A.",
+      "S..S..S"
+    ]
+
+    assert solve(parse(input1)) == 8
+
+    # same but with M instead
+
+    input2 = [
+      "SMMSMMS",
+      "MAMAMAM",
+      "MMMMMMM",
+      "SAMXMAS",
+      "MMMMMMM",
+      "MAMAMAM",
+      "SMMSMMS"
+    ]
+
+    assert solve(parse(input2)) == 8
+  end
+
+  @tag :isolate
+  test "solve_example" do
+    input3 = [
+      "....XXMAS.",
+      ".SAMXMS...",
+      "...S..A...",
+      "..A.A.MS.X",
+      "XMASAMX.MM",
+      "X.....XA.A",
+      "S.S.S.S.SS",
+      ".A.A.A.A.A",
+      "..M.M.M.MM",
+      ".X.X.XMASX"
+    ]
+
+    assert solve(parse(input3)) == 18
+  end
 end
