7ee33905426800efd5415ad495a27a5cc490879c
brendanmc6 <brendanmc6@users.noreply.github.com>
2025-01-05 15:23:03 -0800

    Day10_P01 solution

---
+++ lib/Day10_P01.ex
@@ -0,0 +1,73 @@
+defmodule Day10_P01 do
+  @behaviour Common.Solver
+
+  def read(path) do
+    File.stream!(path)
+    |> Enum.map(&String.trim/1)
+  end
+
+  def parse(rowstrs) do
+    coords =
+      rowstrs
+      |> Enum.with_index()
+      |> Enum.flat_map(fn {strlist, y} ->
+        strlist
+        |> String.split("", trim: true)
+        |> Enum.map(&String.to_integer/1)
+        |> Enum.with_index()
+        |> Enum.map(fn {val, x} -> {{x, y}, val} end)
+      end)
+
+    trailheads = Enum.filter(coords, fn {_c, val} -> val == 0 end) |> Enum.map(&elem(&1, 0))
+
+    {Map.new(coords), trailheads}
+  end
+
+  def next_coord(dir, {x, y}) do
+    case dir do
+      :north -> {x, y - 1}
+      :east -> {x + 1, y}
+      :south -> {x, y + 1}
+      :west -> {x - 1, y}
+    end
+  end
+
+  # boundary case, target coord not found
+  def find_peaks(_prev, nil, _coord, _grid) do
+    []
+  end
+
+  # not passable case
+  def find_peaks(prev, height, _coord, _grid) when height - prev != 1 do
+    []
+  end
+
+  # base case, peak found, return it's coord
+  def find_peaks(_prev, height, coord, _grid) when height == 9 do
+    [coord]
+  end
+
+  # recurse case, walk in all directions
+  def find_peaks(_prev, height, coord, grid) do
+    Enum.flat_map([:north, :south, :east, :west], fn dir ->
+      next_coord = next_coord(dir, coord)
+
+      find_peaks(
+        height,
+        Map.get(grid, next_coord, nil),
+        next_coord,
+        grid
+      )
+    end)
+  end
+
+  def solve({grid, trailheads}) do
+    trailheads
+    |> Enum.map(fn coord ->
+      find_peaks(-1, 0, coord, grid)
+    end)
+    |> Enum.map(&MapSet.new(&1))
+    |> Enum.map(&MapSet.size(&1))
+    |> Enum.sum()
+  end
+end
---
+++ test/Day10_P01_test.exs
@@ -0,0 +1,40 @@
+defmodule Day10_P01.Test do
+  use ExUnit.Case
+  import Day10_P01
+
+  test "parse" do
+    input = [
+      "89010123",
+      "78121874",
+      "87430965",
+      "96549874",
+      "45678903",
+      "32019012",
+      "01329801",
+      "10456732"
+    ]
+
+    {grid, trailheads} = parse(input)
+
+    assert grid[{0, 0}] == 8
+    assert grid[{7, 7}] == 2
+    assert length(trailheads) == 9
+  end
+
+  @tag :isolate
+  test "solve" do
+    input = [
+      "89010123",
+      "78121874",
+      "87430965",
+      "96549874",
+      "45678903",
+      "32019012",
+      "01329801",
+      "10456732"
+    ]
+
+    result = solve(parse(input))
+    assert result == 36
+  end
+end
